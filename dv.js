e698b997b2b834094497192524ff9e08901aae05
utibe.dennis <utibe.dennis@gmail.com>
2020-10-17 15:01:42 +0100

    menu

--- dv.js
+++ dv.js
@@ -1,8 +1,8 @@
 let toggleButton = document.querySelector("#barBtn");
-let menuDisplay = document.querySelector(".menu ");
+let menudisplay = document.querySelector("#menuDisplay");
 
 toggleButton.addEventListener("click", toggleBtn);
 
 function toggleBtn() {
-    menuDisplay.classList.toggle('hide');
+    menudisplay.classList.toggle('hide');
 }
